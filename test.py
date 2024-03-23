1f04e1dc850823eeb6879b0a81aba19218b213f8
Acryptic <acrypticc@gmail.com>
2024-03-23 22:16:50 +0100

    Hello World

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
 #Test Program
+#Number=1 return 1
+#Number=2 return 1+4
+#Number=3 return 1+4+9
+
+
+def squaredNum(number):
+    total=0
+    for num in range(1,number+1):
+        total=total+num**2
+    return total
+
+print(squaredNum(70))
